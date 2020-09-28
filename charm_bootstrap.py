14704951fc1519727f8e88bbd6d007c9105dc576
Andres Ramirez <andres.ramirez.morales@cern.ch>
2020-09-28 21:07:37 +0900

    normal test

--- charm_bootstrap.py
+++ charm_bootstrap.py
@@ -12,23 +12,26 @@ from sklearn.utils import resample
 # call module for plots
 import data_visualization as dv
 
-# input parameters 
-param_o = np.array([0.00, 0.00, 1.00, 1.00, 1.00, 1.00, 1.00]) # coef infront omega_lambda, omega_rho
-param_x = np.array([0.75, 3.75, 0.75, 3.75, 0.75, 3.75, 3.75]) # coef infront A
-param_y = np.array([0.00, 0.00, -1.0, -2.5, 0.50, -1.0, 1.50]) # coef infront B
-param_z = np.array([3.33, 3.33, 3.33, 3.33, 3.33, 3.33, 3.33]) # coef infront G
-#exp_m   = np.array([2695, 2766, 3000, 3050, 3066, 3090, 3188]) # experimental masses
+import data_preparation as dp
 
-def model(w,x,y,z,o,a,b,g):
-    return 2505 + w*o + x*a + y*b + z*g
+# bootstrap
+from sklearn.utils import resample
 
-def least_squares(o, a, b, g):
+
+states = 'All' # All, omega, cascades, sigma_lamb
+# input parameters
+param_v,param_w,param_x,param_y,param_z,mass_sum = dp.fetch_data(states)
+
+def model(mass_sum,v,w,x,y,z,k,a,b,e,g):
+    return mass_sum + v*k + w*a + x*b + y*e + z*g
+
+def least_squares(k, a, b, e, g):
     yvar = 0.01
-    comp_m = model(param_o, param_x, param_y, param_z, o, a, b, g)
-    return np.sum((comp_m - exp_m)**2 / yvar)
+    pred_m = model(mass_sum, param_v, param_w, param_x, param_y, param_z, k, a, b, e, g)
+    return np.sum((pred_m - exp_m)**2 / (yvar**2)) #**2
 
 def fit(least_squares):
-    m = Minuit(least_squares, o=0, a=0, b=0, g=0, error_o=1, error_a=1, error_b=1, error_g = 1, errordef=1)
+    m = Minuit(least_squares, k=0, a=0, b=0, e=0, g=0, error_k=1, error_a=1, error_b=1, error_e=1, error_g=1, errordef=1)
     m.migrad()
     return m
 
@@ -36,43 +39,109 @@ def sample_gauss(mu, sigma):
     return np.random.normal(mu, sigma, 10000)
 
 def random(sample):
-    return np.random.choice(sample, size=1).mean()
-
+    return np.random.choice(sample, size=None)
 
 # arrays to store the sampled parameters
-sampled_o,sampled_a,sampled_b,sampled_g = ([]),([]),([]),([])
+sampled_k,sampled_a,sampled_b,sampled_e,sampled_g,sampled_masses = ([]),([]),([]),([]),([]),([])
+
+# arrays to stores sampled correlation coeficients
+rho_ak,rho_bk,rho_ba,rho_ek,rho_ea,rho_eb,rho_gk,rho_ga,rho_gb,rho_ge=([]),([]),([]),([]),([]),([]),([]),([]),([]),([])
 
 # gaussian pdf with the measured value and with experimental uncertainty
-gauss_2695 = sample_gauss(2695.0, 2.0000)
-gauss_2770 = sample_gauss(2766.0, 2.0000)
+# Omega states
+gauss_2695 = sample_gauss(2695.0, 2.0)
+gauss_2770 = sample_gauss(2766.0, 2.0)
 gauss_3000 = sample_gauss(3000.4, 0.3742)
 gauss_3050 = sample_gauss(3050.2, 0.3317)
 gauss_3066 = sample_gauss(3065.6, 0.4359)
 gauss_3090 = sample_gauss(3090.2, 0.6557)
-gauss_3180 = sample_gauss(3188.0, 13.923)
+# Cascades six-plet
+gauss_2578 = sample_gauss(2578.0, 2.9)
+gauss_2645 = sample_gauss(2645.9, 0.6)
+gauss_2923 = sample_gauss(2923.1, 0.4)
+gauss_2938 = sample_gauss(2938.6, 0.3)
+gauss_2964 = sample_gauss(2964.9, 0.3)
+# Sigma c
+gauss_2453 = sample_gauss(2453.9, 0.14)
+gauss_2518 = sample_gauss(2518.0, 2.3)
+gauss_2801 = sample_gauss(2801.0, 6)
+# Lambda C
+gauss_2286 = sample_gauss(2286.5, 0.14)
+gauss_2592 = sample_gauss(2592.3, 0.28)
+gauss_2628 = sample_gauss(2625.0, 0.19)
+# Cascade C anti-3-plet
+gauss_2469 = sample_gauss(2469.0, 4)
+gauss_2792 = sample_gauss(2792.0, 3.3)
+gauss_2816 = sample_gauss(2815.0, 0.20)
 
 
-# construct the simulated sampling distribution (boostrap technique)
-for _ in range(100000):
+# construct the simulated sampling distribution (bootstrap technique)
+for _ in range(10000):
     # experimental sampled masses
-    exp_m = np.array([random(gauss_2695), random(gauss_2770), random(gauss_3000),
-                      random(gauss_3050), random(gauss_3066), random(gauss_3090),
-                      random(gauss_3180)])
+    if(states=='All'):
+        exp_m = np.array([random(gauss_2695), random(gauss_2770), random(gauss_3000),
+                          random(gauss_3050), random(gauss_3066), random(gauss_3090),
+                          random(gauss_2578), random(gauss_2645), random(gauss_2923),
+                          random(gauss_2938), random(gauss_2964), random(gauss_2453),
+                          random(gauss_2518), random(gauss_2801), random(gauss_2286),
+                          random(gauss_2592), random(gauss_2628), random(gauss_2469),
+                          random(gauss_2792), random(gauss_2816)])
+        
+    elif(states=='omega'):
+        exp_m = np.array([random(gauss_2695), random(gauss_2770), random(gauss_3000),
+                          random(gauss_3050), random(gauss_3066), random(gauss_3090)])
 
+    elif(states=='cascades'):
+        exp_m = np.array([random(gauss_2578), random(gauss_2645), random(gauss_2923),
+                          random(gauss_2938), random(gauss_2964), random(gauss_2469),
+                          random(gauss_2792), random(gauss_2816)])
+
+    elif(states=='sigma_lamb'):
+        exp_m = np.array([random(gauss_2453), random(gauss_2518), random(gauss_2801),
+                          random(gauss_2286), random(gauss_2592), random(gauss_2628)])
+
+               
     # perform the parameter fitting (via minimizing squared distance)
     m = fit(least_squares)
 
-    sampled_o = np.append(sampled_o, m.values['o'])
+    sampled_k = np.append(sampled_k, m.values['k'])
     sampled_a = np.append(sampled_a, m.values['a'])
     sampled_b = np.append(sampled_b, m.values['b'])
+    sampled_e = np.append(sampled_e, m.values['e'])
     sampled_g = np.append(sampled_g, m.values['g'])
 
-print(np.mean(sampled_o),np.mean(sampled_a),np.mean(sampled_b),np.mean(sampled_g))
-print(np.std(sampled_o),np.std(sampled_a),np.std(sampled_b),np.std(sampled_g))
+    # correlation matrix
+    corr = m.np_matrix(correlation=True)
+
+    rho_ak = np.append(rho_ak, corr[1,0])
+    rho_bk = np.append(rho_bk, corr[2,0])
+    rho_ba = np.append(rho_ba, corr[2,1])
+    rho_ek = np.append(rho_ek, corr[3,0])
+    rho_ea = np.append(rho_ea, corr[3,1])
+    rho_eb = np.append(rho_eb, corr[3,2])
+    rho_gk = np.append(rho_gk, corr[4,0])
+    rho_ga = np.append(rho_ga, corr[4,1])
+    rho_gb = np.append(rho_gb, corr[4,2])
+    rho_ge = np.append(rho_ge, corr[4,3])
+
+    # store the sampled masses
+    sampled_masses = np.append(sampled_masses, exp_m)
 
 # plot the simulated sampling distribution,
 # under the Central Limit Theorem, it is expected normal
-dv.plot(sampled_o,'o','Parameter omega','Charm')
+dv.plot(sampled_k,'k','Parameter omega','Charm')
 dv.plot(sampled_a,'a','Parameter A','Charm')
 dv.plot(sampled_b,'b','Parameter B','Charm')
+dv.plot(sampled_e,'e','Parameter E','Charm')
 dv.plot(sampled_g,'g','Parameter G','Charm')
+dv.mass_prediction(param_v, param_w, param_x, param_y, param_z, mass_sum,
+                   np.mean(sampled_k),np.std(sampled_k),np.mean(sampled_a),np.std(sampled_a),
+                   np.mean(sampled_b),np.std(sampled_b),np.mean(sampled_e),np.std(sampled_e),
+                   np.mean(sampled_g),np.std(sampled_g), states)
+
+dv.correlation_matrix(rho_ak,rho_bk,rho_ba,rho_ek,rho_ea,rho_eb,rho_gk,rho_ga,rho_gb,rho_ge, states)
+
+dv.sampled_prediction(mass_sum, param_v, param_w, param_x, param_y, param_z,
+                      sampled_k, sampled_a, sampled_b, sampled_e, sampled_g,
+                      rho_ak,rho_bk,rho_ba,rho_ek,rho_ea,rho_eb,rho_gk,rho_ga,rho_gb,rho_ge)
+
--- data_visualization.py
+++ data_visualization.py
@@ -8,7 +8,9 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.stats import norm
+from scipy.stats import norm, normaltest, shapiro, chisquare, kstest
+from statsmodels.stats.diagnostic import lilliefors
+from oct2py import octave
 
 # sample plots
 def plot(sample,name,xlabel,quark):
@@ -18,12 +20,198 @@ def plot(sample,name,xlabel,quark):
     xmin, xmax = plt.xlim()
     x = np.linspace(xmin, xmax, 100)
     y = norm.pdf(x, np.mean(sample), np.std(sample))
+    p, alpha = normal_test(sample, alpha=0.05, verbose=True)
     plt.plot(x,y,label='Gaussian fit')
-    plt.text(0.15, 0.9,'$\mu$={}, $\sigma$={}'.format(round(np.mean(sample),1), round(np.std(sample),1)),
+    plt.text(0.15, 0.9,'$\\mu$={}, $\\sigma$={}'.format(round(np.mean(sample),1), round(np.std(sample),1)),
+             ha='center', va='center', transform=ax.transAxes)
+    plt.text(0.15, 0.8,'$p_{{val}}$={}, $\\alpha$={}'.format(round(p,3), alpha),
              ha='center', va='center', transform=ax.transAxes)
     plt.legend(frameon=False)
     plt.xlabel(xlabel)
     plt.ylabel('Arbitrary Units')
     plt.title(quark+' mesons')
-    plt.savefig('./plots/'+quark+'_bootstrap_'+name+'.png')
+    plt.savefig('./plots/'+quark+'_bootstrap_'+name+'.pdf')
     plt.close()
+
+
+# mass prediction
+def mass_prediction(v_param, w_param, x_param, y_param, z_param, sum_mass,
+                    Kp, delta_Kp, A, delta_A, B, delta_B, E, delta_E, G, delta_G, name):
+
+    LA = linear_algebra(name)
+    f = open('./tables/parameters_'+name+'.tex', "w")
+    
+    K = np.sqrt(Kp)/1000000000
+    print("\\begin{tabular}{c | c  c  c  c  c }\hline \hline", file=f)
+    print("          & $K$        & $A$             & $B$     & $E$        & $G$            \\\ \hline", file=f)
+    print("Paper     & 5727.12$\pm x.xx$ & 21.54$\pm 0.37$ & 23.91$\pm 0.31$ & 30.34 $\pm 0.23$ & 54.37$\pm 0.58$ \\\ ", file=f)
+    print("Sampled   &",round(Kp,1),' $\\pm',round(delta_Kp,2),'$ &', round(A,2),' $\\pm',round(delta_A,2),'$ &', round(B,2),' $\\pm',round(delta_B,2),'$ &', round(E,2), ' $\\pm',round(delta_E,2),'$ &', round(G,2), ' $\\pm',round(delta_G,2),'$ \\\ ', file=f)
+    print("L.Algebra &",round(LA[0][0],1),' &', round(LA[1][0],2),' &', round(LA[2][0],2),' &', round(LA[3][0],2), '&', round(LA[4][0],2), ' \\\ ', file=f)
+    print("\hline\hline", file=f)
+    print("\end{tabular}", file=f)
+    print("\caption{Model pararameters in MeV, for states: $", name, "$}",file=f)
+    print("    ")
+    f.close()
+    #print(Kp/5727.12, A/21.54, B/23.91, E/30.34,G/54.37)
+    #print(np.std(sampled_a),np.std(sampled_b),np.std(sampled_e),np.std(sampled_g))
+
+    quantum, old, exp, delta_exp = values()
+
+    f = open('./tables/masses_'+name+'.tex', "w")
+    print("\\begin{tabular}{c | c  c  c  c  c}\hline \hline", file=f)
+    print("Mass State & Experiment  &   Predicted mass  &    Predicted mass & diff pred & diff sampl\\\ ", file=f)
+    print("           & (MeV)       &   old (MeV)       &    sampled (MeV)  &     (\\%) &     (\\%)  \\\ \hline", file=f)
+    # print("Mass State & Experiment  &   Predicted mass  &    Predicted mass \\\  ")
+    # print("           & (MeV)       &   old (MeV)       &    sampled (MeV) \\\ \hline  ")
+
+    for i in range(len(v_param)):
+        mass = sum_mass[i] + Kp*v_param[i] + A*w_param[i] + B*x_param[i] + E*y_param[i] + G*z_param[i]
+        error = np.sqrt( (delta_Kp*v_param[i])**2 + (delta_A*w_param[i])**2 + (delta_B*x_param[i])**2 + (delta_E*y_param[i])**2 + (delta_G*z_param[i])**2 )
+        color = "red"
+        old_exp = exp[i]-old[i]
+        new_exp = exp[i]-mass
+        old_exp_abs = np.abs(1 - old[i] / exp[i])
+        new_exp_abs = np.abs(1 - mass / exp[i])        
+        if(old_exp_abs > new_exp_abs):
+            color = "blue"
+            
+        print(quantum[i], exp[i], '$\\pm', delta_exp[i],'$  &', old[i], '$\\pm xx$  &', '\\textcolor{'+color+'}{', round(mass,1),' $\\pm',round(error,1),'$}  &  ',
+              round(old_exp,1), '(', round(old_exp_abs*100,1),')  &  ', round(new_exp,1), '(',round(new_exp_abs*100,1),') \\\  ', file=f)
+       # print(quantum[i], exp[i], '$\\pm', delta_exp[i],'$  &', old[i], '$\\pm xx$  &', '\\textcolor{'+color+'}{', round(mass,1),' $\\pm',round(error,1),'$}  \\\  ')
+
+    print('\hline \hline', file=f)
+    print('\end{tabular}', file=f)
+    print("\caption{Masses in MeV, for states:", name, "}",file=f)
+
+
+
+def values():
+    quantum = ['$\\vert ssc,1/2,1/2,0,0,10/3 \\rangle$ &',   '$\\vert ssc,3/2,3/2,0,0,10/3 \\rangle$ &',   '$\\vert ssc,1/2,1/2,1,0,10/3 \\rangle$ &',
+               '$\\vert ssc,1/2,3/2,1,0,10/3 \\rangle$ &',   '$\\vert ssc,3/2,1/2,1,0,10/3 \\rangle$ &',   '$\\vert ssc,1/2,1/2,0,0,10/3 \\rangle$ &',
+               '$\\vert suc,1/2,1/2,0,1/2,10/3 \\rangle$ &', '$\\vert suc,3/2,3/2,0,1/2,10/3 \\rangle$ &', '$\\vert suc,1/2,3/2,1,1/2,10/3 \\rangle$ &',
+               '$\\vert suc,3/2,1/2,1,1/2,10/3 \\rangle$ &', '$\\vert suc,3/2,3/2,1,1/2,10/3 \\rangle$ &',
+               '$\\vert uuc,1/2,1/2,0,1,10/3 \\rangle$ &',   '$\\vert uuc,3/2,3/2,0,1,10/3 \\rangle$ &',   '$\\vert uuc,1/2,1/2,1,1,10/3 \\rangle$ &',
+               '$\\vert udc,1/2,1/2,0,0,4/3  \\rangle$ &',   '$\\vert udc,1/2,1/2,1,0,10/3 \\rangle$ &',   '$\\vert udc,3/2,1/2,1,0,4/3  \\rangle$ &',
+               '$\\vert ssc,1/2,1/2,0,1/2,4/3 \\rangle$ &',  '$\\vert ssc,1/2,1/2,1,1/2,4/3 \\rangle$ &',  '$\\vert ssc,3/2,1/2,1,1/2,10/3 \\rangle$ &' ]
+
+    old = np.array([2702.4, 2767.0, 3015.8, 3044.5, 3051.6, 3080.4, 2570.1, 2634.8, 2934.1, 2941.2,
+                    2969.9, 2453.1, 2517.7, 2819.0, 2283.7, 2649.7, 2685.6, 2461.2, 2796.5, 2832.4])
+    exp = np.array([2695.0, 2766.0, 3000.4, 3050.2, 3065.6, 3090.2, 2578.0, 2645.9, 2923.0, 2938.5,
+                    2964.9, 2453.9, 2518.0, 2801.0, 2286.5, 2592.3, 2625.0, 2469.0, 2792.0, 2815.0])
+    #delta_exp = np.array([2.0,2.0,0.3742,0.3317,0.4359,0.6557,2.9,0.6,0.4,0.3,0.3,0.14,2.3,6,0.14,0.28,0.19,4,3.3,0.20])
+    delta_exp = np.array([2.0,2.0,0.4,0.3,0.4,0.7,2.9,0.6,0.4,0.3,0.3,0.1,2.3,6.0,0.1,0.4,0.2,4.0,3.3,0.2])
+
+    return quantum, old, exp, delta_exp
+
+def correlation_matrix(rho_ak,rho_bk,rho_ba,rho_ek,rho_ea,rho_eb,rho_gk,rho_ga,rho_gb,rho_ge,name):
+
+    rho_ak=round(np.mean(rho_ak),2)
+    rho_bk=round(np.mean(rho_bk),2)
+    rho_ba=round(np.mean(rho_ba),2)
+    rho_ek=round(np.mean(rho_ek),2)
+    rho_ea=round(np.mean(rho_ea),2)
+    rho_eb=round(np.mean(rho_eb),2)
+    rho_gk=round(np.mean(rho_gk),2)
+    rho_ga=round(np.mean(rho_ga),2)
+    rho_gb=round(np.mean(rho_gb),2)
+    rho_ge=round(np.mean(rho_ge),2)
+
+    
+    f = open('./tables/correlation_'+name+'.tex', "w")
+    print("\\begin{tabular}{c  c  c  c  c  c}\hline \hline", file=f)
+    print("     &  $K$      &     $A$   &      $B$   &      $E$  & $G$ \\\ \hline", file=f)
+    print(" $K$ &     1     &           &            &           &   \\\ ", file=f)
+    print(" $A$ &",rho_ak, "&      1    &            &           &   \\\ ", file=f)
+    print(" $B$ &",rho_bk, "&", rho_ba,"&      1     &           &   \\\ ", file=f)
+    print(" $E$ &",rho_ek, "&", rho_ea,"&", rho_eb, "&      1    &   \\\ ", file=f)
+    print(" $G$ &",rho_gk, "&", rho_ga,"&", rho_gb, "&", rho_ge,"& 1 \\\ \hline \hline", file=f)
+    print('\end{tabular}', file=f)
+
+
+def sampled_prediction(sum_mass, v_param, w_param, x_param, y_param, z_param,
+                       sampled_k, sampled_a, sampled_b, sampled_e, sampled_g,
+                       rho_ak,rho_bk,rho_ba,rho_ek,rho_ea,rho_eb,rho_gk,rho_ga,rho_gb,rho_ge):
+
+    dummy = ([])
+    
+    for i in range(len(sampled_k)):
+        mass = sum_mass[0] + sampled_k[i]*v_param[0] + sampled_a[i]*w_param[0] + sampled_b[i]*x_param[0] + sampled_e[i]*y_param[0] + sampled_g[i]*z_param[0]
+        dummy = np.append(dummy,mass)
+        
+    plot(dummy,'mass','mass','charm')
+    print(dummy.mean(), dummy.std(ddof=1), len(dummy))
+    dummy = np.sort(dummy)
+
+    fK = v_param[0]
+    fA = w_param[0]
+    fB = x_param[0]
+    fE = y_param[0]
+    fG = z_param[0]
+
+    rho_ak = np.mean(rho_ak)
+    rho_bk = np.mean(rho_bk)
+    rho_ba = np.mean(rho_ba)
+    rho_ek = np.mean(rho_ek)
+    rho_ea = np.mean(rho_ea)
+    rho_eb = np.mean(rho_eb)
+    rho_gk = np.mean(rho_gk)
+    rho_ga = np.mean(rho_ga)
+    rho_gb = np.mean(rho_gb)
+    rho_ge = np.mean(rho_ge)
+
+    delta_K = np.std(sampled_k,ddof=1)
+    delta_A = np.std(sampled_a,ddof=1)
+    delta_B = np.std(sampled_b,ddof=1)
+    delta_E = np.std(sampled_e,ddof=1)
+    delta_G = np.std(sampled_g,ddof=1)
+        
+    error_diag  =  (delta_K*fK)**2 + (delta_A*fA)**2 + (delta_B*fB)**2 + (delta_E*fE)**2 + (delta_G*fG)**2
+    error_off1  =  fA*fK*rho_ak*delta_A*delta_K + fB*fK*rho_bk*delta_B*delta_K + fB*fA*rho_ba*delta_B*delta_A + fE*fK*rho_ek*delta_E*delta_K
+    error_off2  =  fE*fA*rho_ea*delta_E*delta_A + fE*fB*rho_eb*delta_E*delta_B + fG*fK*rho_gk*delta_G*delta_K + fG*fA*rho_ga*delta_G*delta_A
+    error_off3  =  fG*fB*rho_gb*delta_G*delta_B + fG*fE*rho_ge*delta_G*delta_E 
+
+    error_total =  np.sqrt( error_diag + 2*(error_off1+error_off2+error_off3))
+
+    print(error_total)
+
+def linear_algebra(name):
+    A = octave.load('./octave/matrix_'+ name + '.mat')
+    b = octave.load('./octave/vector_'+ name + '.mat')
+    return octave.linsolve(A,b)
+
+
+def normal_test(sample,alpha,verbose):
+
+    # Shapiro-Wilk
+    stat, p = shapiro(sample)
+    if verbose:
+        if p > alpha: print('Shapiro this is Gaussian', p)
+        else:         print('Shapiro this is NOT Gaussian', p)
+
+    # chisquare
+    stat, p = chisquare(sample)
+    if verbose:
+        if p > alpha: print('Chisquare this is Gaussian', p)
+        else:         print('Chisquare this is NOT Gaussian', p)
+
+    # lilliefors
+    stat, p = lilliefors(sample)
+    if verbose:
+        if p > alpha: print('Lilliefors this is Gaussian', p)
+        else:         print('Lilliefors this is NOT Gaussian', p)
+
+    # kolmogorov
+    stat, p = kstest(sample, 'norm')
+    if verbose:
+        if p > alpha: print('Kolmogorov this is Gaussian', p)
+        else:         print('Kolmogorov this is NOT Gaussian', p)
+
+    # Angostino
+    k2, p = normaltest(sample)
+    if verbose:
+        if p > alpha: print('Angostino this is Gaussian', p)
+        else:         print('Angostino this is NOT Gaussian', p)    
+
+    return p,alpha
+
+
